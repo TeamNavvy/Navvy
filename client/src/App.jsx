0082cda10a7ee0306d1836c9a2ff4efc864caf31
naoki <jera.hq.devtr.mem009@outlook.jp>
2026-03-03 17:43:36 +0900

    style: body全体へのCSSの適用

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./router/Router";
-import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme, ChakraProvider } from "@chakra-ui/react";
 import "./App.css";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: "orange.100",
+      },
+    },
+  },
+});
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <BrowserRouter>
         <Router></Router>
       </BrowserRouter>
